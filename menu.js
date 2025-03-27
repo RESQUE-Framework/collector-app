1ecef73f4592ff2460ff262e7d9af1d376ae24b0
Alp Kaan Aksu <hi@alpkaanaksu.com>
2025-03-27 17:07:23 +0100

    Push new menu.js

--- menu.js
+++ menu.js
@@ -1,19 +1,45 @@
-async function menu() {
+function pickAccordingToConfig(packPre, config) {    
+    if (config.exclude) {
+        return pickExclude(packPre, ...config.exclude);
+    } else if (config.include) {
+        return pick(packPre, ...config.include);
+    } else {
+        return pick(packPre);
+    }
+}
+
+async function menu(config) {
+    const metaConfig = {
+        min_indicators_warning_threshold: config.meta.config.min_indicators_warning_threshold
+    }
+
+    const metaPre = await use(metaConfig, ...config.meta.sources);
+
+    const pubConfig = {
+        min_indicators_warning_threshold: config.pubs.config.min_indicators_warning_threshold
+    }
+
+    const pubPre = await use(pubConfig, ...config.pubs.sources);
+
+    const softwareConfig = {
+        min_indicators_warning_threshold: config.software.config.min_indicators_warning_threshold
+    }
+
+    const softwarePre = await use(softwareConfig, ...config.software.sources);
+
+    const dataConfig = {
+        min_indicators_warning_threshold: config.data.config.min_indicators_warning_threshold
+    }
+
+    const dataPre = await use(dataConfig, ...config.data.sources);
+
     return {
-        meta: pick(await use({
-            minIndicatorsWarningThreshold: 0
-        }, "core-meta")),
+        meta: pickAccordingToConfig(metaPre, config.meta),
 
-        pub: pick(await use({
-            minIndicatorsWarningThreshold: 5
-        }, "core-pubs")),
+        pub: pickAccordingToConfig(pubPre, config.pubs),
 
-        software: pick(await use({
-            minIndicatorsWarningThreshold: 0
-        }, "core-software")),
+        software: pickAccordingToConfig(softwarePre, config.software),
 
-        data: pick(await use({
-            minIndicatorsWarningThreshold: 5
-        }, "core-data"))
+        data: pickAccordingToConfig(dataPre, config.data)
     }
 }
